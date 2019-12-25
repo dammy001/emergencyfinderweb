8ec2c04090aa60bb6196caa1966438a2a5368a93
dammy001 <damilareanjorin1@gmail.com>
2019-12-25 09:13:24 +0100

    update database

--- config/database.php
+++ config/database.php
@@ -45,12 +45,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'emergfinder'),
+            'username' => env('DB_USERNAME', 'emergencyfinder1'),
+            'password' => env('DB_PASSWORD', 'emergencyfinder1'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
